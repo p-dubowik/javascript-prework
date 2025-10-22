c2ab81db02a83b84c3d246111134acb7dd7c3fbd
Paweł Dubowik <p.dubowik27@gmail.com>
2025-10-22 20:15:50 +0200

    Add entire RockPaperScissors functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let RandomNumber = Math.floor(Math.random() * 3 + 1)
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+let computerMove = `unknown`;
+let playerMove = 'unknown';
 
 function printMessage(msg){
 	let div = document.createElement('div');
@@ -12,4 +15,49 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+console.log('Liczba '+RandomNumber);
+
+if(RandomNumber == 1){
+    computerMove = 'Kamień';
+}
+else if(RandomNumber == 2){
+    computerMove = 'Papier';
+}
+else{
+    computerMove = 'Nożyce';
+}
+
+printMessage('Mój ruch to '+computerMove);
+
+if (playerInput == 1) {
+    playerMove = 'Kamień';
+} else if (playerInput == 2) {
+    playerMove = 'Papier';
+} else if(playerInput == 3){
+    playerMove = 'Nożyce';
+}
+
+console.log(playerMove);
+printMessage('Twój ruch to '+playerMove);
+
+if (computerMove == 'Kamień' && playerMove == 'Papier') { //cpu == kamień
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'Kamień' && playerMove == 'Kamień'){
+    printMessage('Remis');
+} else if(computerMove == 'Kamień' && playerMove == 'Nożyce'){
+    printMessage('Ja wygrywam!');
+}else if(computerMove == 'Papier' && playerMove == 'Kamień'){ //cpu == papier
+    printMessage('Ja wygrywam!');
+}else if(computerMove == 'Papier' && playerMove == 'Papier'){
+    printMessage('Remis');
+}else if(computerMove == 'Papier' && playerMove == 'Nożyce'){
+    printMessage('Ty wygrywasz!');
+}else if(computerMove == 'Nożyce' && playerMove == 'Kamień'){ //cpu == nożyce
+    printMessage('Ty wygrywasz');
+}else if(computerMove == 'Nożyce' && playerMove == 'Papier'){
+    printMessage('Ja wygrywam!');
+}else if(computerMove == 'Nożyce' && playerMove == 'Nożyce'){
+    printMessage('Ty wygrywasz!');
+}else{
+    printMessage('Coś poszło nie tak :(');
+}
